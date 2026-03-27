c97ec7e484152121c878fe807eb4bb88e921ef57
aluno <e023aluno@outlook.com>
2026-03-27 16:11:51 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Servidor rodando na nuvem🚀☁️🚀");
+});
+
+app.get('/teste', (req, res) => {
+  res.send("Rota de teste funcionando!");
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log("Servidor rodando")
+});
